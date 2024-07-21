4ef42c66f1529ccf524ce15cd9d40b6c67b2c091
Creator-De-Coatrack <creatordecoatrack@gmail.com>
2024-07-21 23:16:11 +0700

    Add files via upload

--- Script.js
+++ Script.js
@@ -121,3 +121,117 @@ function slideIt() {
           //do nothing
         }
 }
+function prev(){
+    if (document.getElementById('radio13').checked == true) {
+        document.getElementById('radio12').click();
+        changeImage(getElementById("image12"));
+  }
+    else if (document.getElementById('radio12').checked == true) {
+        document.getElementById('radio11').click();
+        changeImage(getElementById("image11"));
+  } 
+    else if (document.getElementById('radio11').checked == true) {
+        document.getElementById('radio10').click();
+        changeImage(getElementById("image10"));
+} 
+    else if (document.getElementById('radio10').checked == true) {
+        document.getElementById('radio9').click();
+        changeImage(getElementById("image9"));
+} 
+    else if (document.getElementById('radio9').checked == true) {
+        document.getElementById('radio8').click();
+        changeImage(getElementById("image8"));
+}
+else if (document.getElementById('radio8').checked == true) {
+    document.getElementById('radio7').click();
+    changeImage(getElementById("image7"));
+} 
+else if (document.getElementById('radio7').checked == true) {
+    document.getElementById('radio6').click();
+    changeImage(getElementById("image6"));
+} 
+else if (document.getElementById('radio6').checked == true) {
+    document.getElementById('radio5').click();
+    changeImage(getElementById("image5"));
+} 
+else if (document.getElementById('radio5').checked == true) {
+    document.getElementById('radio4').click();
+    changeImage(getElementById("image4"));
+}
+else if (document.getElementById('radio4').checked == true) {
+    document.getElementById('radio3').click();
+    changeImage(getElementById("image3"));
+} 
+else if (document.getElementById('radio3').checked == true) {
+    document.getElementById('radio2').click();
+    changeImage(getElementById("image2"));
+} 
+else if (document.getElementById('radio2').checked == true) {
+    document.getElementById('radio1').click();
+    changeImage(getElementById("image1"));
+} 
+else if (document.getElementById('radio1').checked == true) {
+    document.getElementById('radio13').click();
+    changeImage(getElementById("image13"));
+} 
+    else {
+      //do nothing
+    }
+}
+function next() {
+    if (document.getElementById('radio1').checked == true) {
+            document.getElementById('radio2').click();
+            changeImage(getElementById("image2"));
+      }
+        else if (document.getElementById('radio2').checked == true) {
+            document.getElementById('radio3').click();
+            changeImage(getElementById("image3"));
+      } 
+        else if (document.getElementById('radio3').checked == true) {
+            document.getElementById('radio4').click();
+            changeImage(getElementById("image4"));
+    } 
+        else if (document.getElementById('radio4').checked == true) {
+            document.getElementById('radio5').click();
+            changeImage(getElementById("image5"));
+    } 
+        else if (document.getElementById('radio5').checked == true) {
+            document.getElementById('radio6').click();
+            changeImage(getElementById("image6"));
+    }
+    else if (document.getElementById('radio6').checked == true) {
+        document.getElementById('radio7').click();
+        changeImage(getElementById("image7"));
+  } 
+    else if (document.getElementById('radio7').checked == true) {
+        document.getElementById('radio8').click();
+        changeImage(getElementById("image8"));
+} 
+    else if (document.getElementById('radio8').checked == true) {
+        document.getElementById('radio9').click();
+        changeImage(getElementById("image9"));
+} 
+    else if (document.getElementById('radio9').checked == true) {
+        document.getElementById('radio10').click();
+        changeImage(getElementById("image10"));
+}
+    else if (document.getElementById('radio10').checked == true) {
+        document.getElementById('radio11').click();
+        changeImage(getElementById("image11"));
+    } 
+    else if (document.getElementById('radio11').checked == true) {
+        document.getElementById('radio12').click();
+        changeImage(getElementById("image12"));
+    } 
+    else if (document.getElementById('radio12').checked == true) {
+        document.getElementById('radio13').click();
+        changeImage(getElementById("image13"));
+    } 
+    else if (document.getElementById('radio13').checked == true) {
+        document.getElementById('radio1').click();
+        changeImage(getElementById("image1"));
+    } 
+        else {
+          //do nothing
+        }
+}
